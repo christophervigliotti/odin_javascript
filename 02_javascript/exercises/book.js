cc6e630befe323bb5c4f1776b86221a7b26399b5
Chris <christopher@christophervigliotti.com>
2024-11-18 17:29:33 -0500

    nov 18

---
+++ 02_javascript/exercises/book.js
@@ -0,0 +1,34 @@
+function Book(title, author, page_count, was_read){
+    this.title = title;
+    this.author = author;
+    this.page_count = page_count;
+    this.was_read = was_read;
+    this.info = function() {
+        var was_read_string = (this.was_read) ? 'read it and wept' : 'not read yet';
+        return (
+            title 
+            + ' by ' 
+            + author
+            + ', '
+            + page_count
+            + ' pages, '
+            + was_read_string
+        );
+    }
+}
+
+hobbit = new Book('The Hobbit', 'J R R Tolkien', 295, true);
+console.log(hobbit.info());
+
+// prototype fun
+console.log(
+    Object.getPrototypeOf(hobbit) === Book.prototype
+);
+console.log(
+    Object.getPrototypeOf(hobbit)
+);
+console.log([
+    hobbit.valueOf(),
+    hobbit.hasOwnProperty('valueOf'),
+    Object.prototype.hasOwnProperty('valueOf')
+]);
