e2eb074402adc756a639b50f7fdce394156936f1
Chris <christopher@christophervigliotti.com>
2025-02-20 15:12:01 -0500

    project: library complete

--- 02_javascript/project_library/library.js
+++ 02_javascript/project_library/library.js
@@ -1,4 +1,9 @@
-// resume on 3 https://www.theodinproject.com/lessons/node-path-javascript-library
+/*
+notes
+    console.log('library.js is in the house');
+    https://www.theodinproject.com/lessons/node-path-javascript-library
+    forcing myself to use vanilla js and not jquery
+*/
 const myLib = []; // a new array
 
 function Book(title, author){
@@ -10,16 +15,55 @@ function addBookToLibrary(title, author){
     myLib.push(new Book(title,author));
 }
 
-function writeLibraryToScreen(lib,div){
-    console.log('Muh Library...');
-    var html = '';
-    lib.forEach(function(value,i){
-        let thisBookString = `${value.title} by ${value.author}`;
-        console.log(thisBookString);
-        html += `<li>${thisBookString}</li>`;
-    });
-    // document.getElementById(div).html(html);
+function addSomeBooks(){
+    addBookToLibrary('The Hobbit','J.R.R. Tolkien');
+    addBookToLibrary('To Kill A Mockingbird','Harper Lee');
+    writeLibraryToScreen(myLib,'whatever');
 }
 
-addBookToLibrary('The Hobbit','J.R.R. Tolkien');
-writeLibraryToScreen(myLib,'whatever');
+function clearForm(){
+    console.log('blarg');
+    var title_field = document.getElementById('title');
+    var author_field = document.getElementById('author');
+    title_field.value = '';
+    author_field.value = '';
+}
+
+function initListeners(){
+    var blarg = document.getElementById("add").addEventListener("click", onAddBookClick);
+}
+
+function onAddBookClick(){
+    var title = document.getElementById('title').value.trim();
+    var author = document.getElementById('author').value.trim();
+    if(formIsValid()){
+        addBookToLibrary(title,author);
+        writeLibraryToScreen();
+        clearForm();
+    }
+}
+
+function formIsValid(){
+    var title = document.getElementById('title').value.trim();
+    var author = document.getElementById('author').value.trim();
+    var formIsValid = title != '' && author != '';
+    return formIsValid;
+}
+
+function writeLibraryToScreen(){    
+    console.log('library.js fn writeLibraryToScreen');
+    var book_list_items = '';
+    var the_id = '#books';
+    myLib.forEach(function(value,i){
+        let thisBookString = `${value.title} by ${value.author}`;
+        book_list_items += `<li>${thisBookString}</li>`;
+    });
+    document.querySelector(the_id).innerHTML = book_list_items;
+}
+
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+        initListeners();
+        addSomeBooks();
+    }
+}
